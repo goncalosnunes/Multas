b7a06be6ef5285d8c6a3d1c09db76bafa2715054
goncalo <goncalosilvan@gmail.com>
2019-04-12 20:09:15 +0100

    Adição automática de tuplos nas tabelas da base de dados

--- Multas/Migrations/Configuration.cs
+++ Multas/Migrations/Configuration.cs
@@ -21,7 +21,7 @@ namespace Multas.Migrations {
          //*********************************************************************
          // adiciona AGENTES
          var agentes = new List<Agentes> {
-            new Agentes {ID=1, Nome="Tânia Vieira", Esquadra="Ourém", Fotografia="TaniaVieira.jpg" },
+            new Agentes {ID=1, Nome="Tânia Vieira", Esquadra="Ourém", Fotografia="TaniaVieira.jpg"},
             new Agentes {ID=2, Nome="António Rocha", Esquadra="Ourém", Fotografia="AntonioRocha.jpg" },
             new Agentes {ID=3, Nome="André Silveira", Esquadra="Abrantes", Fotografia="AndreSilveira.jpg" },
             new Agentes {ID=4, Nome="Lurdes Vieira", Esquadra="Leiria", Fotografia="LurdesVieira.jpg" },
